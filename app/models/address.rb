a99604180f01187c5b54f8abd47f0fc7ee16faf0
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-08 03:47:51 +0000

    Done.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ActiveRecord::Base
   belongs_to :person
+
+  def person_attributes=(peson)
+    self.peson = Peson.find_or_create_by(name: peson[:name])
+    self.peson.update(peson)
+  end
 end
